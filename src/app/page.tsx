328e1e6fb12f32ffbb19eccf978af4ef508f6232
Aditya Atyam <atyamx@gmail.com>
2025-02-12 02:39:56 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1 className="text-4xl sm:text-6xl font-bold">aditya atyam</h1>
+      </main>
+    </div>
+  );
+}
