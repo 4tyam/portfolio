85a71949ab33453a2270633e73e32fc31c959306
Aditya Atyam <68732181+4tyam@users.noreply.github.com>
2026-01-03 14:08:40 +0530

    Add email link to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -18,7 +18,10 @@ export default function Footer() {
             {/* <SocialLink href="https://twitter.com/4tyam" label="X" />
             <SocialLink href="https://github.com/4tyam" label="github" />
             <SocialLink href="https://linkedin.com/in/atyam" label="linkedin" /> */}
-            <a className="text-sm sm:text-base">A4 Labs LLC</a>
+            {/* <a className="text-sm sm:text-base">A4 Labs LLC</a> */}
+            <a className="text-sm sm:text-base" href="mailto:a@aty.am">
+              a@aty.am
+            </a>
           </div>
         </div>
       </div>
